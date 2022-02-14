741a579b38f464173bf4eb295951a5deb3a100fa
nlaabid <nlaabid@sampo.uef.fi>
2022-02-14 12:35:32 +0200

    new branch to downgrade environment

---
+++ command.sh
@@ -0,0 +1,11 @@
+conda create --name test-tf
+conda activate test-tf
+conda install python
+python3 -m pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.14.0-py3-none-any.whl
+
+tensorflow-1.14.0-cp36-cp36m-manylinux1_x86_64.whl
+
+pip install tensorflow_probability=0.7.0
+pip install scipy
+pip install pandas
+pip install matplotlib
