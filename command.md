cd7359105fd7507a166dbd991a3f3728749810eb
nlaabid <nlaabid@sampo.uef.fi>
2022-02-15 14:47:45 +0200

    added command.md file

--- command.md
+++ command.md
@@ -1,6 +1,18 @@
 # Install Python dependencies from conda environment
 
+- Run: `conda env create --name <custom_name> --file environment.yml`
+
+Note: you can choose a `<custom_name>`for the environment or run the command above without specifying a name, in which case it will use the environment file specified in the yaml file.
+
 # Install scphere from repository
+
+- From the main directory of the repository, run: `python setup.py install`.
+
+# Check Python installations
+
+- Check Python installations by executing the script: `python example/script/demo.py`
 
 # Install R dependencies from the script
 
+- Run: `Rscript install-densitycut.r`
+- Run `plot.ipynb`to test the R installation.
--- install-densitycut.r
+++ install-densitycut.r
@@ -1,23 +1,20 @@
-# install needed packages
+# install standalone packages from cran
 
 install.packages("robustbase", dependencies = TRUE, repos = "http://cran.us.r-project.org")
 install.packages("sphereplot", dependencies = TRUE, repos = "http://cran.us.r-project.org")
-install.packages("devtools", dependencies = TRUE, repos = "http://cran.us.r-project.org")
 
 # install densitycut from Github repo
 ## clone repo
 install.packages("git2r", dependencies = TRUE, repos = "http://cran.us.r-project.org")
 library(git2r)
+clone(url = "https://github.com/SamGG/densitycut.git", local_path = "densitycut")
+setwd("densitycut")
 
-
+## install densitycut using devtools
+install.packages("devtools", dependencies = TRUE, repos = "http://cran.us.r-project.org")
 library(devtools)
-
-
 build()
 install()
-# quit()
 
-# ragg
-# pkgdown
-# required package: usethis
-# scphere 
+unlink('densitycut')
+unlink('densitycut_0.0.1.tar.gz')
