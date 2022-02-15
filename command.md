f0e6f367b136b200d7d85886d48f22e36275a959
nlaabid <nlaabid@sampo.uef.fi>
2022-02-15 14:34:24 +0200

    removed command.sh

---
+++ command.md
@@ -0,0 +1,6 @@
+# Install Python dependencies from conda environment
+
+# Install scphere from repository
+
+# Install R dependencies from the script
+
--- install-densitycut.r
+++ install-densitycut.r
@@ -1,9 +1,18 @@
-# https://kbroman.org/pkg_primer/pages/build.html
-# go to package directory before running the following
+# install needed packages
+
 install.packages("robustbase", dependencies = TRUE, repos = "http://cran.us.r-project.org")
 install.packages("sphereplot", dependencies = TRUE, repos = "http://cran.us.r-project.org")
 install.packages("devtools", dependencies = TRUE, repos = "http://cran.us.r-project.org")
+
+# install densitycut from Github repo
+## clone repo
+install.packages("git2r", dependencies = TRUE, repos = "http://cran.us.r-project.org")
+library(git2r)
+
+
 library(devtools)
+
+
 build()
 install()
 # quit()
